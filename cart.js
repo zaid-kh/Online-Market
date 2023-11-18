e79b3b57745061208f4be6bd8fa4581821a75aa3
hananhi <hananhimoni@gmail.com>
2023-11-18 19:57:06 +0200

    cart

--- cart.js
+++ cart.js
@@ -3,8 +3,17 @@ const url='https://6555cde784b36e3a431e5f45.mockapi.io/';
 const purchase=document.getElementById('purchase');
 
 
-let shoppingCart = [{productId:1,quantity:2},
-                    {productId:2,quantity:1}];
+let shoppingCart = [{productId:1,
+    quantity:2 ,
+     img:new URL('https://th.bing.com/th/id/R.e50992d3b22d01f34fc854916980e4af?rik=mMELiKX0ZhdeWg&pid=ImgRaw&r=0'),
+     name:'product 1',
+     price:30+'IL'
+},
+{productId:2,
+    quantity:1,
+     img: new URL('https://i.pinimg.com/originals/3b/5f/32/3b5f328152b41414857051b82e5d5c0c.jpg'),
+     name:'product2',
+price:40+'IL'}];
 
 
 
@@ -36,6 +45,21 @@ function displayCart(shoppingCart) {
         deleteProduct.classList.add('deleteProduct')
         deleteProduct.textContent='delete';
         deleteProduct.addEventListener('click',()=>remove_func(item.productId))
+
+        const img = document.createElement("img")
+       img.classList.add('img')
+        img.src=item.img
+        cartItem.appendChild(img);
+
+        const name = document.createElement("h3")
+        name.classList.add('name')
+        name.textContent=item.name;
+        cartItem.appendChild(name);
+
+        const price = document.createElement("section")
+        price.classList.add('price');
+        price.textContent=item.price ;
+       
         
        
         cartItem.appendChild(deleteProduct);
